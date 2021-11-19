38b36dd98f980ea6f593b90f9a015f3adf353bbb
Johannes Schmatz <johannes_schmatz@gmx.de>
2021-11-19 22:44:10 +0100

    add gradient to swap to white mode, add option to disable shooting

--- properties.py
+++ properties.py
@@ -4,9 +4,9 @@ class DriveArea:
     width = 38#cm
 
 class ReflectionMeasurement:
-    # f(x) = m * x + c = 1.990049751243781 * x + -16.50082918739635
-    m = 1.990049751243781
-    c = -16.50082918739635
+    # f(x) = m * x + c = 1.96078431372549 * x + -16.01307189542484
+    m = 1.96078431372549
+    c = -16.01307189542484
     v = 10
 
 class Ports:
@@ -19,6 +19,7 @@ class Ports:
     distance_sensor = Port.S3
 
 class Brick:
+    shoot = True
     sensor_width = 2#cm
     is_silent = False
     tps = 20
@@ -32,8 +33,10 @@ class MotorControl:
     main_motor_full_speed = 10
 
 class DriveSetting:
-    v = 1000
+    v = 300
     center = 50 # the center in percent from black to white
     Kp = 2.5
     Ki = 0
     Kd = -0.5
+
+    from_gradient_to_white = 30 # difference to think its the white area
